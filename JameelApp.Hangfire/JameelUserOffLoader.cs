e924c6cb2cf686db953724646769f2ac58ef3c19
iazer <ibrahim.samir.ayad@hotmail.com>
2023-09-14 05:26:22 +0300

    add hangfire offloader

---
+++ JameelApp.Hangfire/JameelUserOffLoader.cs
@@ -0,0 +1,24 @@
+﻿// Ignore Spelling: Jameel App Hangfire
+
+using Hangfire;
+using JameelApp.Application.Contracts;
+using JameelApp.Application.Contracts.JameelUserDto;
+
+namespace JameelApp.Hangfire
+{
+    public class JameelUserOffLoader : IJameelUserOffLoader
+    {
+        private readonly IJameelUserApplicationService _jameelUserApplicationService;
+        public JameelUserOffLoader(IJameelUserApplicationService jameelUserApplicationService)
+        {
+            _jameelUserApplicationService = jameelUserApplicationService;
+        }
+        public async Task InsertIntoDatabase(JameelUserRequestDto input)
+        {
+            await Task.Run(() => BackgroundJob
+            .Schedule(() =>
+            _jameelUserApplicationService.Add(input),
+            TimeSpan.FromMinutes(5)));
+        }
+    }
+}
