ee6a678bf20399940e7f5915128f41d62101c56e
iazer <ibrahim.samir.ayad@hotmail.com>
2023-09-14 08:35:53 +0300

    add broadcasting architect

--- JameelApp.SignalR/Program.cs
+++ JameelApp.SignalR/Program.cs
@@ -1,6 +1,17 @@
+using JameelApp.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+
+
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+
+
+app.MapHub<JameelUserHubBroadcaster>("jameel-broadcasting");
 app.Run();
